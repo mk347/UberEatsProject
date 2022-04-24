94dbf0c087419fdd9e74d6ea2eeeb754ce760abc
michael.kotylo <michael.kotylo@mrm.com>
2022-04-24 15:25:44 -0400

    Init AWS Amplify

--- UberEatsUser/App.js
+++ UberEatsUser/App.js
@@ -2,6 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import RootNavigator from './src/navigation';
 import { NavigationContainer } from '@react-navigation/native';
+import { Amplify } from 'aws-amplify'
+import config from './src/aws-exports'
+
+Amplify.configure(config)
 
 export default function App() {
   return (
